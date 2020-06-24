2588aee48f63794665d53e6b9c7308a5fd1a86cd
Dario Baumberger <dario.baumberger@gmail.com>
2020-06-24 16:59:36 +0200

    added theme logo swtich

--- public/js/controllers/theme.js
+++ public/js/controllers/theme.js
@@ -1,7 +1,9 @@
 export class Theme {
   constructor() {
     this.$html = document.querySelector("html");
+    this.$logo = document.querySelector(".logo img");
     this.$toggles = document.querySelectorAll(".js-theme--toggle");
+    this.dark = false;
   }
 
   toggleTheme() {
@@ -9,6 +11,16 @@ export class Theme {
       toggle.parentNode.classList.toggle("hidden");
     });
     this.$html.classList.toggle("theme-dark");
+    this.dark = !this.dark;
+    this.toggleLogo();
+  }
+
+  toggleLogo() {
+    if (this.dark) {
+      this.$logo.src = "/images/todo-logo-negative.png";
+    } else {
+      this.$logo.src = "/images/todo-logo.png";
+    }
   }
 
   initEventHandlers() {
